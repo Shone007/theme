fd0f8901cfe048a6e9ffbb866b4da49bca35c29e
Shone007 <bojanicneso@gmail.com>
2016-06-14 22:24:22 +0200

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,5 +1,6 @@
+var woot = document.getElementById("woot"),
 function themechange () {
+    woot.style.backgroundColor = "#2f4fad";
     $('woot').css('background', '#2f4fad');
 });
 themechange();
-
