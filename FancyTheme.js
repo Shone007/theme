db399aed06856a9b63c11a9da32372c7b185ef6c
Shone007 <bojanicneso@gmail.com>
2016-06-15 17:59:20 +0200

    Update FancyTheme.js

--- FancyTheme.js
+++ FancyTheme.js
@@ -98,39 +98,3 @@ addGlobalStyle('#button-dj-quit.button-dj, #button-dj-quit.button-dj {background
 //user avatar in room, not sticking yet.
 //addGlobalStyle('#button-user-avatar, #button-user-avatar {background-image: url("http://i.imgur.com/25rO8.png")!important;}');
 
-//THE WORD REPLACEMENT CODE BELOW IS NOT MINE, IT BELONGS TO JOE SIMMONS
-
-var words = {
-// Syntax: 'Search word' : 'Replace word',
-"points" : "BITS",
-"USERS" : "PONIES",
-"WOOT!" : "BROHOOF!",
-"Now Playing" : "DJ-Pon3's listening to",
-"":""};
-
-String.prototype.prepareRegex = function() {
-return this.replace(/([\[\]\^\&\$\.\(\)\?\/\\\+\{\}\|])/g, "\\$1");
-};
-
-function isOkTag(tag) {
-return (",pre,blockquote,code,input,button,textarea".indexOf(","+tag) == -1);
-}
-
-var regexs=new Array(),
-	replacements=new Array();
-for(var word in words) {
-if(word != "") {
-regexs.push(new RegExp("\\b"+word.prepareRegex().replace(/\*/g,'[^ ]*')+"\\b", 'gi'));
-replacements.push(words[word]);
-}
-}
-
-var texts = document.evaluate(".//text()[normalize-space(.)!='']",document.body,null,6,null), text="";
-for(var i=0,l=texts.snapshotLength; (this_text=texts.snapshotItem(i)); i++) {
-	if(isOkTag(this_text.parentNode.tagName.toLowerCase()) && (text=this_text.textContent)) {
-	for(var x=0,l=regexs.length; x<l; x++) {
-	text = text.replace(regexs[x], replacements[x]);
-	this_text.textContent = text;
-	}
-	}
-}
